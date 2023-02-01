55613ba3a6eec16c991235afc35a9823f559c7f0
Sayed Ali Husain <s0ali@outlook.com>
2023-02-01 14:16:41 +0300

    Initial Commit

---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Sayed Ali Husain on 01/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
